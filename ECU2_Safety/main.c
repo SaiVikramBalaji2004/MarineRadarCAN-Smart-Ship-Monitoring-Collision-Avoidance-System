8823d5e81615b576076a4b64e5bf7e70a9ce1804
Sai Vikram Balaji B <146016892+SaiVikramBalaji2004@users.noreply.github.com>
2026-08-19 19:52:48 +0530

    Update main.c

--- ECU2_Safety/main.c
+++ ECU2_Safety/main.c
@@ -1,16 +1,4 @@
-/*
- * ---------------------------------------------------------------------------
- *  Marine RadarCAN - ECU-2 main : Ship Safety Monitoring Node
- *  Samples the engine temperature and the hazard sensors and broadcasts
- *  the results on CAN2.
- *
- *  Frame  ID      Data[0]
- *  0x104  temp    0..150 deg C (engine temperature)
- *  0x105  fire    0x00 SAFE / 0xFF fire alarm
- *  0x106  gas     0x00 SAFE / 0xFF gas alarm
- *  0x107  water   0x00 SAFE / 0xFF flooding
- * ---------------------------------------------------------------------------
- */
+
 #include "ecu2.h"
 
 static CAN_Message_t msg;

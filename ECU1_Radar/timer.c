235cc1157e5907db25e0b78e8ee79146b5c6713a
Sai Vikram Balaji B <146016892+SaiVikramBalaji2004@users.noreply.github.com>
2026-08-19 19:36:39 +0530

    Update timer.c

--- ECU1_Radar/timer.c
+++ ECU1_Radar/timer.c
@@ -3,20 +3,20 @@
 
 void DelayMs(unsigned int ms)
 {
-    T1PR  = 15000 - 1;            /* 1 ms per tick @ 15 MHz PCLK          */
-    T1TCR = 0x03;                 /* reset counter                        */
-    T1TCR = 0x01;                 /* enable counter                       */
-    while (T1TC < ms);            /* wait for the required time           */
-    T1TCR = 0x03;                 /* reset counter                        */
-    T1TCR = 0x00;                 /* disable counter                      */
+    T1PR  = 15000 - 1;            // 1 ms per tick @ 15 MHz PCLK          
+    T1TCR = 0x03;                 // reset counter                        
+    T1TCR = 0x01;                 // enable counter                       
+    while (T1TC < ms);            // wait for the required time           
+    T1TCR = 0x03;                 // reset counter                        
+    T1TCR = 0x00;                 // disable counter                      
 }
 
 void DelayUs(unsigned int us)
 {
-    T1PR  = 15 - 1;               /* 1 us per tick @ 15 MHz PCLK          */
-    T1TCR = 0x03;                 /* reset counter                        */
-    T1TCR = 0x01;                 /* enable counter                       */
-    while (T1TC < us);            /* wait for the required time           */
-    T1TCR = 0x03;                 /* reset counter                        */
-    T1TCR = 0x00;                 /* disable counter                      */
+    T1PR  = 15 - 1;               // 1 us per tick @ 15 MHz PCLK          
+    T1TCR = 0x03;                 // reset counter                        
+    T1TCR = 0x01;                 // enable counter                       
+    while (T1TC < us);            // wait for the required time           
+    T1TCR = 0x03;                 // reset counter                        
+    T1TCR = 0x00;                 // disable counter                      
 }

2ef64280c5a3899a73cdf69cfc7a67484fefb888
Sai Vikram Balaji B <146016892+SaiVikramBalaji2004@users.noreply.github.com>
2026-08-19 19:59:41 +0530

    Update can.c

--- ECU3_Master/can.c
+++ ECU3_Master/can.c
@@ -1,25 +1,8 @@
-/*
- * ---------------------------------------------------------------------------
- *  ECU-3 CAN1 receive driver : acceptance filter + interrupt reception
- *  CAN1 : RD1 = P0.25 (PINSEL1[19:18] = 01), TD1 = dedicated pin on LPC2129
- *
- *  The acceptance filter is programmed to accept only the individual
- *  standard identifiers 0x101 .. 0x107 (all other bus traffic is dropped).
- *  Received frames update the shared telemetry variables in the ISR.
- * ---------------------------------------------------------------------------
- */
 #include "ecu3.h"
 
 #define AF_RAM_BASE ((volatile unsigned long *)0xE0038000UL)
-
-/* Individual standard-ID table entry : CONTROLLER# bits[15:13],
-   DISABLE bit[12], identifier bits[10:0] (see UM10114 Fig.67).          */
 #define AF_STD_ENTRY(controller, id) \
     (((unsigned long)(controller) << 13) | (unsigned long)(id))
-
-/* CAN controller number as used inside the LUT tables.
-   1 = CAN1 on no-suffix /00 LPC2129 devices.
-   Note: on /01 devices the controllers are numbered 0..n-1, so change to 0. */
 #define AF_CAN1_CONTROLLER  1
 
 /* ---------------- shared telemetry variables --------------------------- */
@@ -35,13 +18,12 @@ void CAN1_Rx_ISR(void) __irq;
 
 void CAN_Init(void)
 {
-    PINSEL1 |= 0x00040000;        /* P0.25 = RD1 (CAN1)                   */
-
-    C1MOD = 0x01;                 /* enter reset mode                     */
-    C1BTR = CAN_BTR_500K;         /* bit timing for 500 kbps              */
+    PINSEL1 |= 0x00040000;        // P0.25 = RD1 (CAN1)                   
+    C1MOD = 0x01;                 // enter reset mode                     
+    C1BTR = CAN_BTR_500K;         // bit timing for 500 kbps              
 
     /* ---------------- configure the acceptance filter ------------------ */
-    AFMR = 0x01;                  /* AccOff : allow filter configuration  */
+    AFMR = 0x01;                  // AccOff : allow filter configuration  
 
     AF_RAM_BASE[0] =  AF_STD_ENTRY(AF_CAN1_CONTROLLER, CAN_ID_OBJ_ANGLE)
                    | (AF_STD_ENTRY(AF_CAN1_CONTROLLER, CAN_ID_OBJ_DIST) << 16);
@@ -50,24 +32,22 @@ void CAN_Init(void)
     AF_RAM_BASE[2] =  AF_STD_ENTRY(AF_CAN1_CONTROLLER, CAN_ID_FIRE_STATUS)
                    | (AF_STD_ENTRY(AF_CAN1_CONTROLLER, CAN_ID_GAS_STATUS) << 16);
     AF_RAM_BASE[3] =  AF_STD_ENTRY(AF_CAN1_CONTROLLER, CAN_ID_WATER_LEVEL)
-                   | (0xFFFFUL << 16);      /* padding entry : disabled   */
+                   | (0xFFFFUL << 16);      // padding entry : disabled   
 
-    SFF_sa     = 0x00;             /* individual std table at RAM base     */
-    SFF_GRP_sa = 0x10;             /* no standard groups                   */
-    EFF_sa     = 0x10;             /* no extended individual entries       */
-    EFF_GRP_sa  = 0x10;            /* no extended groups                   */
-    ENDofTable = 0x10;             /* 4 words -> end of table              */
-
-    AFMR = 0x00;                  /* enable acceptance filter              */
-
-    C1MOD = 0x00;                 /* request to leave reset mode           */
-    while (C1MOD & 0x01);         /* wait until controller leaves reset    */
+    SFF_sa     = 0x00;            // individual std table at RAM base     
+    SFF_GRP_sa = 0x10;            // no standard groups                   
+    EFF_sa     = 0x10;            // no extended individual entries       
+    EFF_GRP_sa  = 0x10;           // no extended groups                   
+    ENDofTable = 0x10;            // 4 words -> end of table              
+    AFMR = 0x00;                  // enable acceptance filter              
+    C1MOD = 0x00;                 // request to leave reset mode           
+    while (C1MOD & 0x01);         // wait until controller leaves reset    
 
     /* ---------------- enable the CAN1 receive interrupt ---------------- */
-    C1IER = 0x01;                 /* RIE : receive interrupt enable        */
+    C1IER = 0x01;                 // RIE : receive interrupt enable        
 
-    VICIntSelect &= ~(1UL << VIC_CAN1_RX);    /* as IRQ                   */
-    VICVectCntl0  = 0x20 | VIC_CAN1_RX;       /* slot 0 -> CAN1 Rx        */
+    VICIntSelect &= ~(1UL << VIC_CAN1_RX);    // as IRQ                   
+    VICVectCntl0  = 0x20 | VIC_CAN1_RX;       // slot 0 -> CAN1 Rx        
     VICVectAddr0  = (unsigned long)CAN1_Rx_ISR;
     VICIntEnable |= (1UL << VIC_CAN1_RX);
 }
@@ -76,11 +56,10 @@ void CAN1_Rx_ISR(void) __irq
 {
     unsigned long rid;
     unsigned char d0;
-
-    if (C1GSR & 0x01)             /* RBS : a message is waiting            */
+    if (C1GSR & 0x01)             // RBS : a message is waiting           \
     {
         rid = C1RID;
-        d0  = (unsigned char)(C1RDA & 0xFF);   /* first data byte         */
+        d0  = (unsigned char)(C1RDA & 0xFF);   // first data byte         
 
         switch (rid)
         {
@@ -94,7 +73,7 @@ void CAN1_Rx_ISR(void) __irq
         default: break;
         }
 
-        C1CMR = 0x04;             /* RRB : release receive buffer          */
+        C1CMR = 0x04;             // RRB : release receive buffer          
     }
-    VICVectAddr = 0x00;           /* end of interrupt                      */
+    VICVectAddr = 0x00;           // end of interrupt                      
 }

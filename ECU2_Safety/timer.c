58a7884633a971b83e9d3713d39a8effa3e8bea0
Sai Vikram Balaji B <146016892+SaiVikramBalaji2004@users.noreply.github.com>
2026-08-19 19:54:49 +0530

    Update timer.c

--- ECU2_Safety/timer.c
+++ ECU2_Safety/timer.c
@@ -1,16 +1,11 @@
-/*
- * ---------------------------------------------------------------------------
- *  ECU-2 Timer helpers (blocking delays) using Timer0
- * ---------------------------------------------------------------------------
- */
 #include "ecu2.h"
 
 void DelayMs(unsigned int ms)
 {
-    T0PR  = 15000 - 1;            /* 1 ms per tick @ 15 MHz PCLK          */
-    T0TCR = 0x03;                 /* reset counter                        */
-    T0TCR = 0x01;                 /* enable counter                       */
-    while (T0TC < ms);            /* wait for the required time           */
-    T0TCR = 0x03;                 /* reset counter                        */
-    T0TCR = 0x00;                 /* disable counter                      */
+    T0PR  = 15000 - 1;            // 1 ms per tick @ 15 MHz PCLK          
+    T0TCR = 0x03;                 // reset counter                        
+    T0TCR = 0x01;                 // enable counter                       
+    while (T0TC < ms);            // wait for the required time           
+    T0TCR = 0x03;                 // reset counter                        
+    T0TCR = 0x00;                 // disable counter                      
 }
